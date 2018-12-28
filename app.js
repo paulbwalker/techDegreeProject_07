1a7859ec7581a0fec8add8ece60136656148154f
Paul Walker <pw97960@gmail.com>
2018-12-27 16:00:14 -0800

    update app.js

--- app.js
+++ app.js
@@ -2,24 +2,28 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'pug');
 
+app.use((req, res, next) => {
+  req.message = 'Page Not Found!';
+  next();
+});
+
 const mainRoutes = require('./routes');
 const projectRoutes = require('./routes/projects');
 
 
-app.use(mainRoutes);
+app.use('/', mainRoutes);
 app.use('/project', projectRoutes);
-
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
+  console.log(req.message);
   next(err);
 });
 
-app.use((err, req, res, next) => {
-  console.log('Not Found');
+app.use((err, req, res, next) => { 
   res.locals.error = err;
-  res.status(err.status);
+  res.status('500');
   res.render('error');
 });
 
